6ed01f9941e677abcd1fbe43e1e4316c5b576461
Peter Barr <50499395+peterbarr@users.noreply.github.com>
2019-06-05 21:51:18 +0100

    Add files via upload
    First python program

---
+++ intro.py
@@ -0,0 +1,9 @@
+
+message = """If you drink water every day your skin will be CLEAR and
+you will feel better. This is a better idea than drinking soda all the 
+time because IT is BAd for you and has too much sugar"""
+
+message = message.replace('you', 'DMT')
+
+print(message)
+
